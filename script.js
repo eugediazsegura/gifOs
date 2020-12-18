748a09b225d26493838d564e7b9754145d100293
Eugenia Diaz Segura <eugenia.diaz@flexxus.net.ar>
2020-12-18 18:26:24 -0300

    proyecto

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ const sailorNight = document.querySelector('#sailorNight')
 const buttonsHover = document.querySelector('.buttons');
 const dropdown = document.querySelector('.dropdown');
 const logo = document.querySelector('#logo');
+const buttonSearch = document.querySelector('#search-submit');
+const inputSearch = document.querySelector('#search-input');
+const home = document.querySelector(".home");
+const lupa = document.querySelector("div.lupa");
+const autocompleteDiv = document.querySelector("div.dropdown.results")
+const autocompeteUl = document.querySelector(".dropdown.results ul")
+const title = document.querySelector("#title1")
+const divTitle = document.querySelector(".principal.subtitle")
+const token = '?api_key=AHbdJlM0sAYa8PSQkZ7MHF4AE6OaePBE';
+const url = 'https://api.giphy.com/v1/gifs';
+const rating = '&rating=pg';
+const suggGif = document.querySelectorAll('.suggestions .gif img');
+const suggButton = document.querySelectorAll('.viewmore a');
+const trendGif = document.querySelector('.trendings');
+const searchGif = document.querySelector('.searchs')
+const search = "/search";
+const limit = '&limit=';
+const q = '&q='
+
 
 
 
@@ -28,6 +47,7 @@ buttonsHover.addEventListener("click", (e) => {
 
 document.addEventListener("scroll", () => {
     dropdown.style.display = "none";
+    autocompleteDiv.classList.add("hidden")
 
 })
 
@@ -38,21 +58,138 @@ sailorNight.addEventListener("click", () => {
 })
 
 sailorDay.addEventListener("click", () => {
+    
     body.classList.remove("night");
     logo.setAttribute("src", "assets/gifOF_logo.png");
     localStorage.setItem('night', false)
 })
 
+logo.addEventListener("click", () => {
+    home.classList.remove("hidden");
+})
+
+inputSearch.addEventListener("focus", ()=> {
+    addClassToButton();
+})
+
+inputSearch.addEventListener("focusout", ()=> {
+    buttonSearch.classList.remove("submit-imput", "nightSubmit-input")
+    buttonSearch.classList.remove("active")
+    lupa.style.removeProperty("background-Image")
+    
+   
+    
+})
 
 
-const token = '?api_key=AHbdJlM0sAYa8PSQkZ7MHF4AE6OaePBE';
-const url = 'https://api.giphy.com/v1/gifs';
-const raiting = '&rating=pg';
-const suggGif = document.querySelectorAll('.suggestions .gif img');
-const suggButton = document.querySelectorAll('.viewmore a');
-const trendGif = document.querySelector('.trendings');
 
 
+
+
+let getSearch = async (query) => {
+    if (!/^[\s|\W]{1,}$/gm.test(query) == true) {
+        const searchURI = [];
+        const getSearchApi = url + search + token + q + query + limit +'31' + '&offset=0' + rating
+        let resultsSearch = await fetch(getSearchApi);
+        let resultsSearchJson = await resultsSearch.json();
+        if (resultsSearchJson.meta.status == 200) {
+            let imagenesSearch = resultsSearchJson.data;
+
+            for (const imagenSearch of imagenesSearch) {
+                searchURI.push({
+                    'src': imagenSearch.images.original.webp
+                });
+            }
+            console.log(resultsSearch);
+            return searchURI;
+
+        } else {
+
+            throw new Error(`No existen GIFs para ${query}`)
+        }
+    }else if(query ==" "){
+        throw new Error(`No existen GIFs para espacio vacio`)
+    } else {
+
+        throw new Error(`No existen GIFs para ${query}`)
+
+    }
+
+}
+
+ let getAutocomplete = async (query) => {
+    const autocompleteURI = [];
+    const getAutocompleteApi = url+ search + "/tags" + token + q + query + limit + '3'+ '&offset=0'
+    let resultsAutocomplete = await fetch(getAutocompleteApi);
+    let autocompleteJson = await resultsAutocomplete.json();
+    let tags = autocompleteJson.data;
+    for (let tag of tags) {
+        autocompleteURI.push({
+            'tag' : tag.name
+        });
+    }
+    return autocompleteURI
+
+} 
+
+
+
+function ejecutarBusqueda(inputValue) {
+    getSearch(inputValue).then((gifs) => {
+        home.classList.add("hidden");
+        searchGif.innerHTML=' ';
+        title.innerHTML=inputValue + " (resultados)"
+            for (let i = 1; i < gifs.length; i++) {
+                let div = document.createElement('div');
+                let img = document.createElement('img');
+                div.classList.add("trend-gif")
+                if (i % 5 == 0 && i > 0) {
+                    div.classList.add("wide")
+                }
+                //div.appendChild(div2);
+                div.appendChild(img);
+                img.setAttribute('src', gifs[i].src);
+                searchGif.appendChild(div)
+                autocompleteDiv.classList.add("hidden")
+                divTitle.style.marginTop = "100px"
+
+
+
+            }
+
+
+        }).catch((error) =>{
+            let divError = document.createElement('div');
+            divError.classList.add("error");
+            divError.innerHTML = error + " Intenta realizar una nueva búsqueda";
+            searchGif.appendChild(divError)
+        })
+}
+
+buttonSearch.addEventListener("click", () => {
+    buttonSearch.classList.add("active")
+    ejecutarBusqueda(inputSearch.value);
+    
+})
+
+inputSearch.addEventListener('keydown', (e) => {
+    if (e.keyCode == '13') {
+        buttonSearch.classList.add("active")
+        ejecutarBusqueda(inputSearch.value);
+       
+    }
+})
+
+buttonSearch.addEventListener("focus", () =>{
+    addClassToButton();
+    buttonSearch.classList.add("active")
+    
+})
+
+buttonSearch.addEventListener("focusout", () =>{
+    buttonSearch.classList.remove("active")
+    
+})
 
 let getsuggestionsGifs = async () => {
 
@@ -100,7 +237,7 @@ let getTrendingGifs = async () => {
     const trend = '/trending';
     const trendURI = [];
 
-    const getTrendingApi = url + trend + token + '&limit=31&rating=pg'
+    const getTrendingApi = url + trend + token + limit + rating
     let resultsTrend = await fetch(getTrendingApi);
     let resultsTrendJson = await resultsTrend.json();
     if (resultsTrendJson.meta.status == 200) {
@@ -113,7 +250,7 @@ let getTrendingGifs = async () => {
         }
         return trendURI;
     } else {
-        throw new Error('Error en Fetch '+ resultsTrendJson.meta.msg)
+        throw new Error('Error en Fetch ' + resultsTrendJson.meta.msg)
     }
 
 
@@ -135,6 +272,61 @@ getTrendingGifs().then(
 
         }
     }
-).catch((error)=> {
+).catch((error) => {
     console.error(error)
-} )
+})
+
+
+/* const token = '?api_key=AHbdJlM0sAYa8PSQkZ7MHF4AE6OaePBE';
+const url = 'https://api.giphy.com/v1/gifs';
+const raiting = '&rating=pg';
+const suggGif = document.querySelectorAll('.suggestions .gif img');
+const suggButton = document.querySelectorAll('.viewmore a');
+const trendGif = document.querySelector('.trendings');
+ */
+
+
+/* let autocomplete = async () => {
+
+    const ids = ["5PhoLTGAiHguInjU8w", "IeKpg7M6wu7W8", "g5OVsXxZlKTz0dCYfE", "26AHG5KGFxSkUWw1i"]
+    const suggestions = "&ids=";
+    let idsURI = [];
+
+    const suggestionsApi = url + token + suggestions + ids.join();
+    let result = await fetch(suggestionsApi);
+    let resultsJson = await result.json();
+    let imagenes = resultsJson.data;
+
+    for (const imagen of imagenes) {
+        idsURI.push({
+            'src': imagen.images.original.webp,
+            'url': imagen.url
+        });
+
+    }
+
+    return idsURI;
+}
+
+getsuggestionsGifs().then(
+    (gifs) => {
+        console.log(gifs);
+        for (let i = 0; i < gifs.length; i++) {
+            suggGif[i].setAttribute('src', gifs[i].src);
+            suggButton[i].setAttribute('href', gifs[i].url)
+
+
+        }
+
+    }); */
+
+    function addClassToButton() {
+        if (localStorage.getItem('night') == "false") {
+            buttonSearch.classList.add("submit-imput")
+            lupa.style.backgroundImage = 'url(/assets/lupa.svg)'
+        } else if (localStorage.getItem('night') == "true") {
+            buttonSearch.classList.add("nightSubmit-input")
+            lupa.style.backgroundImage = 'url(/assets/lupa_light.svg)'
+        }
+        
+    }
