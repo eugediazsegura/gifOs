34801f7cecd4f1e6e57e9c847a48091446214521
Euge <eugeniaa.ds@gmail.com>
2021-01-04 14:39:48 -0300

    video upload

--- video/script-video.js
+++ video/script-video.js
@@ -8,8 +8,8 @@ const video = document.querySelector('video');
 const capturar = document.querySelector('#button4')
 
 
-let grabar;
-let grabando = false;
+let recorder;
+let recording = false;
 
 logo.addEventListener("click", () =>{
     window.location = "../index.html"
@@ -37,6 +37,8 @@ start.addEventListener("click", ()=>{
 })
 
 function getStreamAndRecord() {
+    //aca empieza a correr la camara, se pide permiso al usuario
+    //chrome lanza un error si la pagina que da el script es insegura
     navigator.mediaDevices.getUserMedia({
             audio: false,
             video: {
@@ -51,13 +53,62 @@ function getStreamAndRecord() {
         .then(function (stream) {
             video.srcObject = stream;
             video.play();
-            capturar.addEventListener('click', startVideoRecord(source));
 
-          
-            
+            capturar.addEventListener('click', () => {
+                recording = !recording;
+
+                if (recording ===true) {
+                    this.disabled = true;
+                    //capturando el video
+                    recorder = RecordRTC(stream, 
+                        {
+                            type: 'gif', 
+                            frameRate : 1, 
+                            quality: 10, 
+                            width: 360,
+                            hidden: 240,
+                            onGifRecordingStarted: function ()
+                            {
+                                console.log("started")
+                            },
+                        }
+                    );
+                    //inicio de grabacion
+                    recorder.startRecording();
+
+                    //getDuration
+                    //cambiar estilos del dom
+                    recorder.camera = stream;
+                    
+                }else{
+                    this.disabled = true;
+                    recorder.stopRecording(stopRecordingCall);
+                    recording = false
+                }
+            });
         });
 };
 
-startVideoRecord(stream){
+function stopRecordingCall(){
+    recorder.camera.stop();
+    //le damos el formato requerido a la data que vamos a enviar como body del POST
 
+    //se crea un archivo form que luego contendrá el gif pasado a formato blob
+    let form = new FormData();
+
+    form.append('file', recorder.getBlob(), 'test.gif');
+    console.log(form.get('file'));
+
+    //este es el boton de cargar gif
+    upload.addEventListener('click', () =>{
+        //css uploarmessage.classlist.remove(hidden);
+        //preview.classlist.add(hidden);
+        uploadGif(form);
+    })
+
+    //obtener el blob de la variable recorder, que actualmente tiene el gif almacenado
+    objectURL = URL.createObjectURL(recorder.getBlob());
+    preview.src = objectURL
 }
+/* startVideoRecord(stream){
+ */
